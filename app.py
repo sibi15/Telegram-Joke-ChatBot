e535a1cff98198b8c456a13563392e0ca99daffa
Sibi Karthik C V <99868966+sibi15@users.noreply.github.com>
2025-05-25 11:54:44 +0400

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()  # load environment variables from .env file
 
+# Python 3.11.6
+
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
 os.environ["LANGCHAIN_TRACING_V2"] = "true"   # used to trace API calls on LangChain
